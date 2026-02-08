ac1760d50a0a81860d7bafb28823624823f960ad
Mark <mark@remarkablemark.org>
2026-02-07 20:06:02 -0500

    test: add snapshot test

--- __tests__/index.test.mts
+++ __tests__/index.test.mts
@@ -4,4 +4,10 @@ describe('index', () => {
   it('exports decycle', () => {
     assert.strictEqual(typeof decycle, 'function');
   });
+
+  it('matches snapshot for circular reference', () => {
+    const array: unknown[] = [];
+    array[0] = array;
+    expect(decycle(array)).toEqual([{ $ref: '$' }]);
+  });
 });
--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -4,4 +4,14 @@ describe('index', () => {
   it('exports decycle', () => {
     assert.strictEqual(typeof decycle, 'function');
   });
+
+  it('matches snapshot for complex circular structure', () => {
+    const obj: Record<string, unknown> = {
+      name: 'root',
+      children: [{ id: 1 }, { id: 2 }],
+    };
+    obj.self = obj;
+    obj.children = [obj.children, obj.children];
+    expect(decycle(obj)).toMatchSnapshot();
+  });
 });
